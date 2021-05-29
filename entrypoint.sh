f5579d3ebdb26695e24c7705fd5969f3aecc1fff
inwardik <volodymyr.syrotkin@gmail.com>
2021-05-29 17:22:19 +0300

    add bin bash

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,5 @@ python manage.py makemigrations --no-input
 
 python manage.py migrate --no-input
 
-python manage.py runserver 0.0.0.0:8000
+exec gunicorn djangoGunicornNginxDocker.wsgi:application -b 0.0.0.0:8000 --reload
+
